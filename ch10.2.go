160eb7cb09eead9fc1066824297268392746edc5
0suyog <dahalsunischit@gmail.com>
2025-07-18 16:44:12 +0545

    chapter 10 l6

--- ch10.2.go
+++ ch10.2.go
@@ -1,23 +1,12 @@
 package main
 
 import (
-	"fmt"
 	"strings"
 )
 
-func removeProfanity(message *string) {
+func _removeProfanity(message *string) {
 	profaneWords:=map[string]string{"fubb":"****","shiz":"****","witch":"*****"}
 	for key,word := range profaneWords{
 		*message=strings.ReplaceAll(*message,key,word)
 	}
-}
-
-func main(){
-testCase:=[]string{
-			"English, motherfubber, do you speak it?",
-			"Oh man I've seen some crazy ass shiz in my time...",
-	}
-
-removeProfanity(&testCase[0])
-fmt.Println(testCase[0])
 }
